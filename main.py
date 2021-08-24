42fe5e28d4f8ee36e72a4424eca366d9a1c226ab
Mot0511 <suvorov.matvej9@gmail.com>
2021-08-24 21:23:38 +0300

    _

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import telebot
 import random
 from datetime import datetime
+import weather
 
 bot = telebot.TeleBot(open('bot.txt', 'r').read())
 
 keyboard = telebot.types.ReplyKeyboardMarkup()
 keyboard.row('Осталось...', 'Давай играть в камень, ножницы, бумага')
+keyboard.row('Какая сейчас погода')
+
 keyboard_kmb = telebot.types.ReplyKeyboardMarkup()
 keyboard_kmb.row('Камень', 'Ножницы', 'Бумага')
+keyboard_kmb.row('Выйти из игры')
 
 
 
@@ -16,8 +20,8 @@ affairs = ['хорошо', 'нормально']
 employ = ['гуляю', 'ем', 'купаюсь', 'сплю', 'с тобой разговариваю', 'играю в БрОуЛь СтАрС', 'играю в Геометри дэш', 'играю в га...ой...в Майнкрафт', 'ничего', 'разговариваю с другим ботом']
 months = ["", "январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
 kmb = ["камень", "ножницы", "бумага"]
-
 is_kmb_game = False
+
 give_comp = ''
 give_user = ''
 
@@ -30,26 +34,29 @@ def el_list(list, el):
     return False
 
 def start_kmb(user):
+    global give_comp
+    global give_user
+
     comp = kmb[random.randint(0, len(kmb) - 1)]
     give_comp = comp
     give_user = user
     if comp == 'камень' and user == 'камень':
-        return 'Ничья'
+        return 2
 
     elif comp == 'камень' and user == 'ножницы':
-        return 'Я выйграл'
+        return 0
 
     elif comp == 'камень' and user == 'бумага':
-        return 'Я проиграл'
+        return 1
 
     elif comp == 'ножницы' and user == 'ножницы':
-        return 'Ничья'
+        return 2
 
     elif comp == 'ножницы' and user == 'бумага':
-        return 'Я выйграл'
+        return 0
 
     elif comp == 'бумага' and user == 'бумага':
-        return 'Ничья'
+        return 2
 
 
 
@@ -57,26 +64,64 @@ date = datetime.now()
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Это бот созданный специално для 6б класса', reply_markup=keyboard)
+    is_kmb_game = False
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
     if message.text.lower() == 'давай играть в камень, ножницы, бумага':
         bot.send_message(message.chat.id, 'Камень ножницы бумага раз два три...', reply_markup=keyboard_kmb)
+        global is_kmb_game
         is_kmb_game = True
 
+    elif message.text.lower() == 'какая сейчас погода':
+        bot.send_message(message.chat.id, "Сейчас в Кирове:" + weather.get_weather() + ' градусов', reply_markup=keyboard)
+
+
+    elif message.text.lower() == 'выйти из игры':
+        is_kmb_game = False
+        bot.send_message(message.chat.id, 'Ты вышел из игры', reply_markup=keyboard)
 
     elif message.text.lower() == 'камень':
-        bot.send_message(message.chat.id, start_kmb('камень'))
-        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+        if is_kmb_game:
+            res = start_kmb('камень')
+            if res == 0:
+                bot.send_message(message.chat.id, "Ты проиграл")
+
+            elif res == 1:
+                bot.send_message(message.chat.id, "Ты выйграл")
+
+            elif res == 2:
+                bot.send_message(message.chat.id, "Ничья")
+
+            bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
 
     elif message.text.lower() == 'ножницы':
-        bot.send_message(message.chat.id, start_kmb('ножницы'))
-        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+        if is_kmb_game:
+            res = start_kmb('ножницы')
+            if res == 0:
+                bot.send_message(message.chat.id, "Ты проиграл")
+
+            elif res == 1:
+                bot.send_message(message.chat.id, "Ты выйграл")
+
+            elif res == 2:
+                bot.send_message(message.chat.id, "Ничья")
+
+            bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
 
     elif message.text.lower() == 'бумага':
-        bot.send_message(message.chat.id, start_kmb('бумага'))
-        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+        if is_kmb_game:
+            res = start_kmb('бумага')
+            if res == 0:
+                bot.send_message(message.chat.id, "Ты проиграл")
 
+            elif res == 1:
+                bot.send_message(message.chat.id, "Ты выйграл")
+
+            elif res == 2:
+                bot.send_message(message.chat.id, "Ничья")
+
+            bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
 
 
 
---
+++ weather.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+session = requests.Session()
+url = 'https://www.gismeteo.ru/weather-kirov-4292/'
+temp = []
+
+def get_weather():
+    request = session.get(url, headers=headers)
+    if request.status_code == 200:
+        # start parsing
+        soup_tmp = bs(request.content, 'lxml')
+
+        temp.append(soup_tmp.find_all('span', attrs={'class': 'js_value tab-weather__value_l'}))
+
+        return temp[0][0].text
