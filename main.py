a14329fc87de1f55d8160bccb16f4a8683ba12d5
Mot0511 <suvorov.matvej9@gmail.com>
2021-08-24 12:13:22 +0300

    _

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import telebot
 import random
 from datetime import datetime
 
-bot = telebot.TeleBot('1979749823:AAFRk_aUPegdWsQ1Z4qBKB7eIxiHs1qX11U')
+bot = telebot.TeleBot(open('bot.txt', 'r').read())
 
 keyboard = telebot.types.ReplyKeyboardMarkup()
-keyboard.row('Осталось...')
+keyboard.row('Осталось...', 'Давай играть в камень, ножницы, бумага')
+keyboard_kmb = telebot.types.ReplyKeyboardMarkup()
+keyboard_kmb.row('Камень', 'Ножницы', 'Бумага')
+
+
 
 hello = ['привет', 'приветствую']
 affairs = ['хорошо', 'нормально']
 employ = ['гуляю', 'ем', 'купаюсь', 'сплю', 'с тобой разговариваю', 'играю в БрОуЛь СтАрС', 'играю в Геометри дэш', 'играю в га...ой...в Майнкрафт', 'ничего', 'разговариваю с другим ботом']
 months = ["", "январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
+kmb = ["камень", "ножницы", "бумага"]
+
+is_kmb_game = False
+give_comp = ''
+give_user = ''
 
 def el_list(list, el):
     list = list.split()
@@ -19,6 +28,31 @@ def el_list(list, el):
             return True
 
     return False
+
+def start_kmb(user):
+    comp = kmb[random.randint(0, len(kmb) - 1)]
+    give_comp = comp
+    give_user = user
+    if comp == 'камень' and user == 'камень':
+        return 'Ничья'
+
+    elif comp == 'камень' and user == 'ножницы':
+        return 'Я выйграл'
+
+    elif comp == 'камень' and user == 'бумага':
+        return 'Я проиграл'
+
+    elif comp == 'ножницы' and user == 'ножницы':
+        return 'Ничья'
+
+    elif comp == 'ножницы' and user == 'бумага':
+        return 'Я выйграл'
+
+    elif comp == 'бумага' and user == 'бумага':
+        return 'Ничья'
+
+
+
 date = datetime.now()
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -26,7 +60,27 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    if el_list(message.text.lower(), 'привет'):
+    if message.text.lower() == 'давай играть в камень, ножницы, бумага':
+        bot.send_message(message.chat.id, 'Камень ножницы бумага раз два три...', reply_markup=keyboard_kmb)
+        is_kmb_game = True
+
+
+    elif message.text.lower() == 'камень':
+        bot.send_message(message.chat.id, start_kmb('камень'))
+        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+
+    elif message.text.lower() == 'ножницы':
+        bot.send_message(message.chat.id, start_kmb('ножницы'))
+        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+
+    elif message.text.lower() == 'бумага':
+        bot.send_message(message.chat.id, start_kmb('бумага'))
+        bot.send_message(message.chat.id, 'Ты дал: ' + give_user + ', а я дал: ' + give_comp)
+
+
+
+
+    elif el_list(message.text.lower(), 'привет'):
         bot.send_message(message.chat.id, hello[random.randint(0, len(hello) - 1)])
 
     elif el_list(message.text.lower(), 'дела'):
